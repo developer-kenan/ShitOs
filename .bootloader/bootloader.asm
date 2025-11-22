8f60588d9e617c7384eb588ebd4fd426528bf0eb
developer-kenan <sagopakajmersf@gmail.com>
2025-11-22 13:53:36 +0300

    bootloader add

---
+++ .bootloader/bootloader.asm
@@ -0,0 +1,59 @@
+use16
+org 0x7C00
+start:
+mov ah, 0x0E
+
+mov al, 'I'
+
+int 0x10
+
+mov al, 'L'
+
+int 0x10
+
+mov al, 'O'
+
+int 0x10
+
+mov al, 'V'
+
+int 0x10
+
+mov al, 'E'
+
+int 0x10
+
+mov al, 'Y'
+
+int 0x10
+
+mov al, 'O'
+
+int 0x10
+
+mov al, 'U'
+
+int 0x10
+
+mov al, 'B'
+
+int 0x10
+
+mov al, 'E'
+
+int 0x10
+
+mov al, 'R'
+
+int 0x10
+
+mov al, 'I'
+
+int 0x10
+
+mov al, 'L'
+
+int 0x10
+jmp start
+times 510 -($-$$) db 0
+dw 0x55AA
