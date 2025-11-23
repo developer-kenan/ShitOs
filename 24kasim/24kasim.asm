fa875f613cb3322c6132fb3920be4ad28875bd2b
developer-kenan <sagopakajmersf@gmail.com>
2025-11-23 21:48:27 +0300

    new 24kasim folders

---
+++ 24kasim/24kasim.asm
@@ -0,0 +1,90 @@
+use16
+org 0x7C00
+start:
+mov ah, 0x0E
+mov al, '2'
+int 0x10
+mov al, '4'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'K'
+int 0x10
+mov al, 'A'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'M'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'G'
+int 0x10
+mov al, 'R'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'T'
+int 0x10
+mov al, 'M'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'N'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'R'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'G'
+int 0x10 
+mov al, 'U'
+int 0x10
+mov al, 'N'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'N'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'Z'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'K'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'T'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'N'
+int 0x10
+mov al, '.'
+int 0x10
+int 0x0
+jmp start
+times 510 -($-$$) db 0
+dw 0x55AA
